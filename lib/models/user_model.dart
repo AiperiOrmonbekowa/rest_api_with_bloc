ea1d550500826a94f9bd216c4220f56bfb18511f
Aiperi Ormonbekova <aiperi.ormonbekovna@gmail.com>
2023-06-09 12:27:57 +0600

    first commit

---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  final int id;
+  final String name;
+  final String Username;
+  final String email;
+  final String phone;
+  final String website;
+
+  UserModel(
+      this.id, this.name, this.Username, this.email, this.phone, this.website);
+}
